389074b097be563f21ef69302e5240ec241c158b
haihuilab <zhanghaihui@pku.edu.cn>
2025-12-02 18:39:56 -0800

    v1.1.1

--- src/scbiot/__about__.py
+++ src/scbiot/__about__.py
@@ -6,7 +6,7 @@ from typing import Tuple
 
 __all__ = ["__version__", "__version_info__"]
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 
 def _parse_version(version: str) -> Tuple[int | str, ...]:
